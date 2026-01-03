86b40b14ba3553951d6397c6e3fcb8dc6f58bbaa
kamilaboch <kamila.bochenska@interia.pl>
2026-01-03 10:43:35 +0100

    Dodano zapis i odczyt profilu gracza

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,51 @@
 #include "raylib.h"
 #include <string>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
 // Stany aplikacji
 enum GameState { MENU, GAME };
 
+// PROFIL
+struct Profile {
+    string nick;
+};
+
+// ZARZ¥DZANIE PROFILAMI 
+class ProfileManager {
+public:
+    vector<Profile> profiles;
+    int selected = 0;
+
+    void load() {
+        ifstream in("profiles.dat");
+        profiles.clear();
+        Profile p;
+        while (in >> p.nick)
+            profiles.push_back(p);
+    }
+
+    void save() {
+        ofstream out("profiles.dat");
+        for (auto& p : profiles)
+            out << p.nick <<"\n";
+    }
+
+    void addProfile(const string& nick) {
+        profiles.push_back({ nick});
+        selected = (int)profiles.size() - 1;
+        save();
+    }
+
+    Profile& current() {
+        return profiles[selected];
+    }
+};
+
 int main() {
     const int W = 1280, H = 720;
-
     InitWindow(W, H, "Flanki Shooter");
     SetTargetFPS(60);
 
@@ -18,56 +55,108 @@ int main() {
     float playerY = H - 60.0f;
     float speed = 400.0f;
 
-    string nick = "";
+    ProfileManager pm;
+    pm.load();
+
+    bool newProfileMode = false;
+    string newNick = "";
 
     while (!WindowShouldClose()) {
         float dt = GetFrameTime();
 
-        // ===== MENU =====
+        // MENU
         if (state == MENU) {
 
-            // Wpisywanie nicku
-            for (int key = GetCharPressed(); key > 0; key = GetCharPressed())
-                if (key >= 32 && key <= 125 && nick.length() < 12)
-                    nick += (char)key;
+            // NOWY PROFIL
+            if (newProfileMode) {
 
-            // Usuwanie znaku
-            if (IsKeyPressed(KEY_BACKSPACE) && !nick.empty())
-                nick.pop_back();
+                for (int key = GetCharPressed(); key > 0; key = GetCharPressed())
+                    if (key >= 32 && key <= 125 && newNick.length() < 12)
+                        newNick += (char)key;
 
-            // Start gry tylko gdy podano nick
-            if (IsKeyPressed(KEY_ENTER) && !nick.empty())
-                state = GAME;
+                if (IsKeyPressed(KEY_BACKSPACE) && !newNick.empty())
+                    newNick.pop_back();
 
+                if (IsKeyPressed(KEY_ENTER) && !newNick.empty()) {
+                    pm.addProfile(newNick);
+                    newNick.clear();
+                    newProfileMode = false;
+                }
+
+            }
+            // WYBÓR PROFILU 
+            else if (!pm.profiles.empty()) {
+                int count = (int)pm.profiles.size();
+                if (IsKeyPressed(KEY_UP))
+                    pm.selected = (pm.selected - 1 + count) % count;
+
+                if (IsKeyPressed(KEY_DOWN))
+                    pm.selected = (pm.selected + 1) % count;
+
+                if (IsKeyPressed(KEY_ENTER)) {
+                    playerX = W / 2.0f;
+                    state = GAME;
+                }
+            }
+
+            if (IsKeyPressed(KEY_N)) {
+                newProfileMode = true;
+                newNick.clear();
+            }
+
+            //  RYSOWANIE MENU
             BeginDrawing();
             ClearBackground(BLACK);
 
-            DrawText("FLANKI SHOOTER", W / 2 - 220, 250, 50, RAYWHITE);
-            DrawText("Nick:", W / 2 - 150, 320, 30, GRAY);
-            DrawRectangle(W / 2 - 150, 360, 300, 40, DARKGRAY);
-            DrawText(nick.c_str(), W / 2 - 140, 370, 25, RAYWHITE);
+            DrawText("FLANKI SHOOTER", W / 2 - 220, 120, 50, RAYWHITE);
+            DrawText("Profile:", W / 2 - 200, 200, 30, GRAY);
+
+            for (int i = 0; i < pm.profiles.size(); i++) {
+                Color c = (i == pm.selected) ? YELLOW : GRAY;
+                DrawText(pm.profiles[i].nick.c_str(),
+                    W / 2 - 200, 240 + i * 30, 22, c);
+            }
+
+            DrawText("ENTER - wybierz", W / 2 - 200, H - 180, 20, DARKGRAY);
+            DrawText("N - nowy profil", W / 2 - 200, H, 20, DARKGRAY);
+            DrawText("ESC - wyjscie", W / 2 -200, H - 20, 20, DARKGRAY);
+            if (newProfileMode) {
+                DrawText("Nowy nick:", W / 2 - 150, 470, 25, GRAY);
+                DrawRectangle(W / 2 - 150, 500, 300, 40, DARKGRAY);
+                DrawText(newNick.c_str(), W / 2 - 140, 510, 25, RAYWHITE);
+            }
 
             EndDrawing();
             continue;
         }
 
-        // ===== GRA =====
-        if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))
-            playerX -= speed * dt;
-        if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))
-            playerX += speed * dt;
+        // GRA 
+        if (!pm.profiles.empty()) {
+            if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))
+                playerX -= speed * dt;
+            if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))
+                playerX += speed * dt;
 
-        if (playerX < 20) playerX = 20;
-        if (playerX > W - 20) playerX = W - 20;
+            if (playerX < 20) playerX = 20;
+            if (playerX > W - 20) playerX = W - 20;
 
-        if (IsKeyPressed(KEY_ESCAPE))
-            state = MENU;
-
+            if (IsKeyPressed(KEY_ESCAPE)) {
+                state = MENU;
+            }
+        }
         BeginDrawing();
         ClearBackground(BLACK);
 
+
         DrawCircle((int)playerX, (int)playerY, 18, RAYWHITE);
-        DrawText(TextFormat("Gracz: %s", nick.c_str()), 20, 20, 20, GRAY);
+
+        if (!pm.profiles.empty()) {
+            DrawText(TextFormat("Gracz: %s", pm.current().nick.c_str()),
+                20, 20, 20, GRAY);
+        }
+        else {
+            DrawText("Brak profili - wcisnij N w menu", 20, 20, 20, GRAY);
+        }
 
         EndDrawing();
     }
